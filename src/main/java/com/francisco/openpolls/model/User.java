fb284468b7418ddfe189086ebc63a2891381ffc7
Francisco Juane <franciscojuaneutn@gmail.com>
2024-12-09 22:55:58 -0300

    Application.properties changed to application.yml. User implementation of UserDetails finished. @Builder added to User

---
+++ src/main/java/com/francisco/openpolls/model/User.java
@@ -0,0 +1,75 @@
+package com.francisco.openpolls.model;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.francisco.openpolls.model.common.Constants;
+import com.francisco.openpolls.model.common.EffectiveModel;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = Constants.TABLE_PREFIX + "USERS")
+@Entity
+public class User extends EffectiveModel implements UserDetails {
+
+	@Column(nullable = false, length=50)
+	private String firstName;
+	
+	@Column(nullable = false, length=50)
+	private String lastName;
+	
+	@Column(nullable = false)
+	private String password;
+	
+	@Column(nullable = false)
+	private String email;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of();
+	}
+
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return this.isEffective();
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	
+}
---
+++ src/main/java/com/francisco/openpolls/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.francisco.openpolls.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.francisco.openpolls.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	Optional<User> findByEmail(String email);
+
+}
