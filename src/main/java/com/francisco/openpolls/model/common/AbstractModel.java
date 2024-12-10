9700701754a0f6eba8d0cacbe4cf9a1f50fee18a
Francisco Juane <franciscojuaneutn@gmail.com>
2024-12-09 22:45:44 -0300

    AbstractModel @CreationTimestamp and @UpdateTimestamp implemented instead of manual methods for creationTime and updateTime

--- src/main/java/com/francisco/openpolls/model/common/AbstractModel.java
+++ src/main/java/com/francisco/openpolls/model/common/AbstractModel.java
@@ -2,14 +2,14 @@ package com.francisco.openpolls.model.common;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Version;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,23 +25,14 @@ public class AbstractModel implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long id;
 	
+	@CreationTimestamp
 	public LocalDateTime creationTime;
 	
+	@UpdateTimestamp
 	public LocalDateTime updateTime;
 	
 	@Version
 	public long version = 1;
 	
-	@PreUpdate
-	private void preUpdate() {
-		updateTime = LocalDateTime.now();
-	}
 
-	@PrePersist
-	private void prePersist() {
-		if (creationTime == null) {
-			creationTime = LocalDateTime.now();
-			updateTime = LocalDateTime.now();
-		}
-	}
 }
